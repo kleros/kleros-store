7be737659963486c04a91f43d738bc54244fc7e7
n1c01a5 <wagner.nicolas1@gmail.com>
2018-01-19 04:15:29 +0000

    Deploy to GitHub pages [ci skip]

---
+++ api_project.js
@@ -0,0 +1,19 @@
+define({
+  "title": "Kleros store",
+  "url": "http://localhost:3000/",
+  "name": "Kleros store",
+  "version": "0.0.1",
+  "description": "API Kleros Store",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-01-19T04:15:28.346Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
