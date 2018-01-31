4b3f139d1819d6969ead0b198f284580b84920a1
n1c01a5 <wagner.nicolas1@gmail.com>
2018-01-31 23:12:27 +0000

    Deploy to GitHub pages [ci skip]

--- api_project.js
+++ api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-01-31T22:58:09.140Z",
+    "time": "2018-01-31T23:12:25.334Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
