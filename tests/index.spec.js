d30b6e81116e0aaa04ea5ac86c2ced8ca71f8871
Sam Vitello <sam.j.vitello@gmail.com>
2018-05-22 14:11:41 -0700

    test: make seed a promise and call before each test suite

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,12 +1,17 @@
 const app = require('../src/app')
+const seed = require('../src/seed')
 const request = require('supertest')
 
 describe('GET /', () => {
+  beforeAll(async () => {
+    await seed()
+  })
+
   test('HTTP status + Api is working fine !', async () => {
     const { statusCode, text } = await request(app).get('/')
 
     expect(statusCode).toBe(200)
-    expect(text).toBe('<!DOCTYPE html><html><head><title>Kleros store (API)</title><link rel=\"stylesheet\" href=\"/stylesheets/style.css\"></head><body><p>Kleros store (API)</p></body></html>')
+    expect(text).toBe('<!DOCTYPE html><html><head><title>Kleros store (API)</title><link rel=\"stylesheet\" href=\"/styles/style.css\"></head><body><p>Kleros store (API)</p></body></html>')
   })
 
   test('gets profile by address', async () => {
@@ -16,5 +21,5 @@ describe('GET /', () => {
     expect(body.address).toBe('profile1')
     expect(body.disputes.length).toBe(2)
     expect(body.contracts.length).toBe(0)
-  })
+  }, 10000)
 })
