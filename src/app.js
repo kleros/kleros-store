aa8810dba204f73348987b0984f7d4e6943b828f
Sam Vitello <sam.j.vitello@gmail.com>
2018-05-22 17:07:23 -0700

    fix: make sure all middleware comes before routes

--- src/app.js
+++ src/app.js
@@ -21,6 +21,13 @@ if (config.seedDb) { seed() }
 const app = express()
 app.disable('x-powered-by')
 
+// set headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  next()
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -32,21 +39,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '../public')))
 
-// Routes
-app.use('/', routes)
-
-// set headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-  next()
-})
-
 // set secret
 app.set('secret', config.secret)
 
 // docs
 app.use('/apidoc', express.static('apidoc'))
+
+// Routes
+app.use('/', routes)
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
