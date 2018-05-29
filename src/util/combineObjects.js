2b3224ef7215dbdbd03f325baf032efdd18a900b
Sam Vitello <sam.j.vitello@gmail.com>
2018-05-29 16:49:40 -0700

    fix: write function to combine immutible object with new fields

---
+++ src/util/combineObjects.js
@@ -0,0 +1,12 @@
+// We want to combine object such that { key: undefined } is replaced by { key: value }
+export const combineObjects = (immutableObj, newObj) => {
+  for (var property in newObj) {
+    if (newObj.hasOwnProperty(property)) {
+      if (!immutableObj[property]) {
+        immutableObj[property] = newObj[property]
+      }
+    }
+  }
+
+  return immutableObj
+}
