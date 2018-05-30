1e2d7793eb5a4d122b9f5f1a72e01a8a4f9058a4
Sam Vitello <sam.j.vitello@gmail.com>
2018-05-30 07:56:16 -0700

    chore: pr comment updates

--- src/util/combineObjects.js
+++ src/util/combineObjects.js
@@ -1,6 +1,6 @@
 // We want to combine object such that { key: undefined } is replaced by { key: value }
 export const combineObjects = (immutableObj, newObj) => {
-  for (var property in newObj) {
+  for (let property in newObj) {
     if (newObj.hasOwnProperty(property)) {
       if (!immutableObj[property]) {
         immutableObj[property] = newObj[property]
