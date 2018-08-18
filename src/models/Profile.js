2e58b3feae0d478ba9e471b2558a30cbfc956b2d
Nicolas Wagner <contact@wagner-nicolas.com>
2018-08-18 11:26:41 +0200

    [feat] add transactionId property

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -10,6 +10,7 @@ const ProfileSchema = new Schema({
   session: Number,
   contracts : [{
     address: String,
+    transactionId: Number,
     email: String,
     evidence: [{
       name: String,
